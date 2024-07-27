0f017c70ecd77e36932be7aedd1aea76bef8e3ed
Phillip Schichtel <phillip@schich.tel>
2024-07-27 12:09:08 +0200

    upgrade dockcross to 20240727-3995c0c

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ val buildReleaseBinaries = project.findProperty("libdatachannel.build-release-bi
 fun DockcrossRunTask.baseConfigure(outputTo: Directory, args: List<String> = emptyList()) {
     group = nativeGroup
 
-    dockcrossTag = "20240529-0dade71"
+    dockcrossTag = "20240727-3995c0c"
     inputs.dir(jniPath)
 
     dependsOn(tasks.compileJava)
