ceaf7ee2fd47224b5d6a341e635251422fe681cc
Phillip Schichtel <phillip@schich.tel>
2024-11-05 00:32:59 +0100

    upgrade openssl to 3.3.2

--- jni/conanfile.py
+++ jni/conanfile.py
@@ -3,7 +3,7 @@ from conan import ConanFile
 
 class LibDataChannel(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    requires = "openssl/3.2.2"
+    requires = "openssl/3.3.2"
     generators = "CMakeDeps"
 
     def configure(self):
